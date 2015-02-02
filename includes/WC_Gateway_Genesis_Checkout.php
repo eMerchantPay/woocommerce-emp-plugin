3605f2b856cdf68d8c59879a36df09e5c897e617
Peter Manchev <p.manchev@emerchantpay.com>
2015-02-02 16:13:46 +0200

    Refactor the Main Checkout class Update the eMerchantPay logo Use unique keyword for WooCommerce hierarchy

--- includes/WC_Gateway_Genesis_Checkout.php
+++ includes/WC_Gateway_Genesis_Checkout.php
@@ -1,11 +1,10 @@
 <?php
 
-// Don't run standalone
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require 'genesis/vendor/autoload.php';
+include_once 'genesis/vendor/autoload.php';
 
 use \Genesis\Genesis as Genesis;
 use \Genesis\GenesisConfig as GenesisConf;
@@ -14,11 +13,11 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 {
 	public function __construct()
 	{
-		$this->id           = 'genesis';
+		$this->id           = 'emerchantpay';
 		$this->has_fields   = false;
 		$this->method_title = __('eMerchantPay', 'woocommerce_emerchantpay');
 		$this->supports     = array( 'products', 'refunds' );
-		$this->icon         = plugins_url( 'assets/images/logo.gif', plugin_dir_path(__FILE__) );
+		$this->icon         = plugins_url( 'assets/images/logo.png', plugin_dir_path(__FILE__) );
 
 		$this->init_form_fields();
 		$this->init_settings();
@@ -31,10 +30,6 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 				$this->$name = $value;
 			}
 		}
-
-		// WooCommerce hooks
-		//add_action('init', array(&$this, 'process_gateway_response' ));
-		//add_action( 'woocommerce_thankyou_' . $this->id, array(&$this, 'process_return' ));
 
 		// WPF Redirect
 		add_action( 'woocommerce_receipt_' . $this->id, array(&$this, 'generate_form' ));
@@ -50,7 +45,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 		}
 
 		// Credentials Setup
-		$this->setGenesisCredentials($this->settings);
+		$this->set_credentials($this->settings);
 	}
 
 	/**
@@ -160,6 +155,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 					break;
 				case 'failure':
 					$order->update_status('failed');
+					wc_add_notice( 'Invalid data, please verify your data again!', 'error' );
 					break;
 				case 'cancel':
 					$order->update_status('cancelled');
@@ -193,7 +189,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 			'cancel'    => $order->get_cancel_order_url(), //sprintf('%s&type=cancel', $order->get_checkout_order_received_url()),
 		);
 
-		$transaciton_id = $this->makeTransactionId($order_id);
+		$transaciton_id = $this->generate_id($order_id);
 
 		$genesis = new Genesis('WPF\Create');
 
@@ -262,7 +258,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 
 		$genesis
 			->request()
-				->setTransactionId($this->makeTransactionId($order_id))
+				->setTransactionId($this->generate_id($order_id))
 				->setUsage($reason)
 				->setRemoteIp($_SERVER['REMOTE_ADDR'])
 				->setReferenceId($order->get_transaction_id())
@@ -275,10 +271,8 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 
 		if ($genesis->response()->isSuccessful()) {
 			$order->add_order_note(
-				__( 'Refunded completed!', 'woocommerce_emerchantpay' ) .
-				"\n" .
-				__( 'Refund ID:', 'woocommerce_emerchantpay') .
-				"\n" .
+				__( 'Refunded completed!', 'woocommerce_emerchantpay' ) . PHP_EOL .
+				__( 'Refund ID:', 'woocommerce_emerchantpay') . PHP_EOL .
 				$response->unique_id
 			);
 
@@ -312,24 +306,18 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 				$reconcile = $genesis->response()->getResponseObject()->payment_transaction;
 
 				if ($reconcile) {
-					$transaction_id = $this->parseTransactionId(strval($reconcile->transaction_id));
+					list($order_id,$salt) = explode('-', $reconcile->transaction_id);
 
-					$order = new WC_Order( $transaction_id['order_id'] );
+					$order = new WC_Order( $order_id );
 
 					switch ( $reconcile->status ) {
 						case 'approved':
 							$amount = \Genesis\Utils\Currency::exponentToReal(strval($reconcile->amount), strval($reconcile->currency));
 
 							$order->add_order_note(
-								__( 'Payment through Genesis completed!', 'woocommerce_emerchantpay' ) .
-								"\n" .
-								__( 'Payment ID:', 'woocommerce_emerchantpay') .
-								"\n" .
-								strval($reconcile->unique_id) .
-								"\n" .
-								__( 'Total:', 'woocommerce_emerchantpay') .
-								' ' .
-								$amount
+								__( 'Payment through Genesis completed!', 'woocommerce_emerchantpay' ) . PHP_EOL .
+								__( 'Payment ID:', 'woocommerce_emerchantpay') . PHP_EOL . strval($reconcile->unique_id) . PHP_EOL .
+								__( 'Total:', 'woocommerce_emerchantpay') . ' ' . $amount
 							);
 
 							$order->payment_complete( strval($reconcile->unique_id) );
@@ -367,7 +355,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 	 *
 	 * @return array|string
 	 */
-	private function makeTransactionId($input)
+	private function generate_id($input)
 	{
 		// Why are we doing this?
 		// We need to be sure that we have a unique string we can use as transaction id.
@@ -379,24 +367,6 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 	}
 
 	/**
-	 * Parse transaction id from a string to assoc array
-	 *
-	 * @param $input
-	 *
-	 * @return array|bool
-	 */
-	private function parseTransactionId($input)
-	{
-		$arr = explode('-', $input);
-
-		// Use @ to silence notices/warnings
-		return array (
-			'order_id' => @$arr[0],
-			'salt'     => @$arr[1],
-		);
-	}
-
-	/**
 	 * Set the Genesis PHP Lib Credentials, based on the customer's
 	 * admin settings
 	 *
@@ -404,7 +374,7 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 	 *
 	 * @return void
 	 */
-	private function setGenesisCredentials($settings = array())
+	private function set_credentials($settings = array())
 	{
 		GenesisConf::setToken( $settings['token'] );
 		GenesisConf::setUsername( $settings['username'] );
@@ -414,25 +384,4 @@ class WC_Gateway_Genesis_Checkout extends WC_Payment_Gateway
 			(isset($settings['test_mode']) && $settings['test_mode']) ? 'sandbox' : 'production'
 		);
 	}
-
-	/*
-	private function searchOrderNotes($order_id, $needle)
-	{
-		remove_filter('comments_clauses', array( 'WC_Comments' ,'exclude_order_comments'), 10, 1 );
-
-		$query = new WP_Comment_Query();
-
-		$comments = $query->query(
-			array(
-				'type'      => 'order_note',
-				'search'    => $needle,
-				'post_id'   => intval($order_id),
-			)
-		);
-
-		add_filter('comments_clauses', array( 'WC_Comments' ,'exclude_order_comments'), 10, 1 );
-
-		return reset($comments);
-	}
-	*/
 }
